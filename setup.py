726cc499bd832afac7d515cfc704038916aabeeb
Teemu Rytilahti <tpr@iki.fi>
2017-10-26 22:44:51 +0200

    fix install_requires to be saner

--- setup.py
+++ setup.py
@@ -25,7 +25,15 @@ setup(
 
     packages=["miio", "mirobo"],
 
-    install_requires=['construct', 'click', 'cryptography', 'pretty_cron', 'typing', 'zeroconf', 'pycrypto', 'attrs', 'android_backup'],
+    install_requires=['construct',
+                      'click',
+                      'cryptography',
+                      'pretty_cron',
+                      'typing',
+                      'zeroconf',
+                      'pycrypto',
+                      'attrs',
+                      'android_backup'],
     entry_points={
         'console_scripts': [
             'mirobo=miio.vacuum_cli:cli',
